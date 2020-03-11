c17d5a3049d1ee79dd2bc6d5b01656844bc3417b
0x0luke <lukejcw@icloud.com>
2020-03-11 14:17:24 +0000

    remove auth.js not used

--- screens/Auth.js
+++
@@ -1,7 +0,0 @@
-import {AsyncStorage} from "react-native";
-
-export const AuthKey = "";
-
-export const Authenticated = () => AsyncStorage.setItem(AuthKey, "true");
-
-export const Unauthenticated = () => AsyncStorage.removeItem(AuthKey);
